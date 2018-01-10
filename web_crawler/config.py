b6ea03bb7c88d129030a43292f5580b2e6717135
majiahui <majiahui@shuqianba.com>
2018-01-10 18:46:50 +0800

    init

--- web_crawler/config.py
+++ web_crawler/config.py
@@ -1,3 +1,3 @@
 #encoding:utf-8
 ding_talk_host="oapi.dingtalk.com"
-ding_talk_url="/robot/send?access_token=xxx"
+ding_talk_url="/robot/send?access_token=744d1e6e8e2e0bd68230aaeafbba965cbd4e968201df9204cf4527a24e0a5dda"
